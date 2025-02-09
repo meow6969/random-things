ecf722a1e8e1daf7ee9be03f95e0d0287af26937
meow6969 <75815262+meow6969@users.noreply.github.com>
2025-02-09 08:12:50 -0800

    add gofasterdiagaonlly mod

---
+++ c-sharp/GoFasterDiagonally/GoFasterDiagonally/ModEntry.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+
+
+namespace GoFasterDiagonally;
+
+internal sealed class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        Harmony harmony = new Harmony("meow.GoFasterDiagonally.patch");
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(Farmer))]
+    [HarmonyPatch(nameof(Farmer.getMovementSpeed))]
+    class Patch01
+    {
+        static void Postfix(ref float __result)
+        {
+            if (Game1.player.movementDirections.Count > 1 && __result != 0f && 
+                (Game1.CurrentEvent == null || Game1.CurrentEvent.playerControlSequence))
+            {
+                __result /= 0.707f;
+            }
+        }
+    }
+}
---
+++ c-sharp/GoFasterDiagonally/README.md
@@ -0,0 +1,8 @@
+# GoFasterDiagonally
+stardew valley mod that makes u go faster diagonally  
+basically in vanilla when u go diagonal ur speed is multiplied by 1/sqrt(2)  
+this normalizes the speed so no matter what direction u go it will always be equal velocity  
+  
+this undoes that so when u go diagonally u go baseSpeed*sqrt(2)  
+its super awesome!!  
+![im a gamer](https://media1.tenor.com/m/sdEjblumcWgAAAAC/omori-kel.gif)
