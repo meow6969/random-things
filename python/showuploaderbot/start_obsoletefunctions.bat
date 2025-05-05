75472bb5acbb98f8cb8e964e0ad5891fea65a6ab
meow6969 <75815262+meow6969@users.noreply.github.com>
2025-05-05 00:17:20 -0700

    fix bugs with cmd scripts aaaaaaaa

--- python/showuploaderbot/start_obsoletefunctions.bat
+++ python/showuploaderbot/start_obsoletefunctions.bat
@@ -1,5 +1,8 @@
 @echo off
 
 call ensure_venv.bat
+call venv\Scripts\activate.bat
 
 python obsoletefunctions.py
+
+set /P meow="press enter to exit..."
--- python/showuploaderbot/start_showsendingserver_client.bat
+++ python/showuploaderbot/start_showsendingserver_client.bat
@@ -1,6 +1,9 @@
 @echo off
 
 call ensure_venv.bat
+call venv\Scripts\activate.bat
 
 cd showsendingserver
 python client.py
+
+set /P meow="press enter to exit..."
